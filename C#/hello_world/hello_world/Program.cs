b9186e88c34ea22c2d2bfc5db1932cf0a856e08b
imdiegodev1 <diegoanr40@hotmail.com>
2023-10-04 18:51:01 -0500

    C# hello world

---
+++ C#/hello_world/hello_world/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//This is the first program you can write in C#. As you can see, it is a simple "Hello Worlde" created
+//by default when you create a new project.
+
+//This is the way to print in console:
+Console.WriteLine("Hello, World!");
+
+
+//btw, this is also the way you create comments in your code
